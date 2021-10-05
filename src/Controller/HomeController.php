c7c77ad825e2155ce98872ec155b4f73e06c6143
bplaniteye <bplaniteye.pro@outlook.fr>
2021-10-05 14:20:50 +0200

    Création de routes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,4 +16,56 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    /**
+     * @Route("/about", name="index_about")
+     */
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+/**
+ * @Route("/programme",name="index_programme")
+ */
+    public function programme(): Response
+    {
+        return $this->render('home/programme.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+ * @Route("/galerie",name="index_galerie")
+ */
+public function galerie(): Response
+{
+    return $this->render('home/galerie.html.twig', [
+        'controller_name' => 'HomeController',
+    ]);
+}
+
+/**
+ * @Route("/actualites",name="index_actualites")
+ */
+public function actualites(): Response
+{
+    return $this->render('home/actualites.html.twig', [
+        'controller_name' => 'HomeController',
+    ]);
+}
+
+
+
+
+
 }
